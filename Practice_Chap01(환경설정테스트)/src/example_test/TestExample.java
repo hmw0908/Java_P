ff8d702be6abd0133646498ae89fca1425ae47a1
hmw0908 <91178833+hmw0908@users.noreply.github.com>
2022-07-06 10:15:52 +0900

    Chap01

---
+++ Practice_Chap01(환경설정테스트)/src/example_test/TestExample.java
@@ -0,0 +1,11 @@
+package example_test;
+
+public class TestExample {
+	//F11: 컴파일 후 실행시키는 단축키
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ㅎㅇ java");
+
+	}
+
+}
