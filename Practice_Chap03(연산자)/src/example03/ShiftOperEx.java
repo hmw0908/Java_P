cbc3d17dd43905f3b1cd6cb30a8e74e855d3931a
hmw0908 <91178833+hmw0908@users.noreply.github.com>
2022-07-11 23:48:07 +0900

    Commit_Oper_Ex03

---
+++ Practice_Chap03(연산자)/src/example03/ShiftOperEx.java
@@ -0,0 +1,15 @@
+package example03;
+
+public class ShiftOperEx {
+
+	public static void main(String[] args) {
+
+		int a = 8;
+		int b = 2;
+		//shift 연산은 그래픽 분야에서 많이 사용된다.
+		//이유 : 고속처리
+		System.out.println("8 << 2 : " + (8<<2));
+		System.out.println("8 >> 2 : " + (8>>2));
+	}
+
+}
---
+++ Practice_Chap03(연산자)/src/example03/ThreeOperEx.java
@@ -0,0 +1,23 @@
+package example03;
+
+import java.util.Scanner;
+
+public class ThreeOperEx {
+
+	public static void main(String[] args) {
+		// 삼항 연산자를 사용하는 방법
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("점수를 입력하세요: ");
+		int score = sc.nextInt();
+		
+		char grade = (score >= 90) ? 'A' : 'B';
+		System.out.println("당신의 등급: " + grade);
+		
+		grade = (score >= 90) ? 'A' : ((score >=80)?'B':'C');
+		System.out.println("당신의 등급: " + grade);
+		
+		sc.close();
+	}
+
+}
