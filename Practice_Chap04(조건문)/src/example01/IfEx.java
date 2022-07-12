939b6751fef7db128157f582a0e5cbc18bbf666d
hmw0908 <91178833+hmw0908@users.noreply.github.com>
2022-07-12 22:52:38 +0900

    Chap04_commit

---
+++ Practice_Chap04(조건문)/src/example01/IfEx.java
@@ -0,0 +1,13 @@
+package example01;
+
+public class IfEx {
+
+	public static void main(String[] args) {
+		int score = 93;
+		if(score > 90) 
+			System.out.println("90보다 크다");
+		
+		else 
+			System.out.println("90미만이다.");
+	}
+}
